430cdb116f6d90aea27f96934f330975bc317f95
Fabrice Cheumen <yannfabricecheumen@gmail.com>
2024-06-10 13:39:29 +0200

    ajout

--- change_status_3rd.php
+++ change_status_3rd.php
@@ -6,7 +6,7 @@ include('dbConnect.php');
 if(isset($_REQUEST['status'])){
     $status = $_REQUEST['status'];
     $id = $_REQUEST['id'];
-
+     
     $sql = "update candidate_3rd set approve_status=:status where id=:id";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':status',$status);
